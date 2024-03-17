80c2e3f7a14a3574a6eaf551bf0c53dd0e852120
Papaya13-03 <Papaya1303@gmail.com>
2024-03-17 14:31:04 +0700

    vercel config

---
+++ api/index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import apiRouter from "../src/routes/api.route.js";
+import userRouter from "../src/routes/user.route.js";
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT | 5000;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+app.use("/auth", userRouter);
+app.use("/api", apiRouter);
+
+app.get("/", (req, res) => {
+  res.json("Welcome to server!");
+});
+
+mongoose
+  .connect(process.env.MONGO_DB_URL)
+  .then(() => {
+    console.log("Database connected!");
+    app.listen(port, () => {
+      console.log(`Server is running on http://localhost:${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log({ err });
+  });
