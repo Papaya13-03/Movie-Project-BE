d7d7e6de44ae78c46f2640727a5561713dc9f638
Papaya13-03 <Papaya1303@gmail.com>
2024-03-17 22:04:27 +0700

    last changeee

--- api/index.js
+++ api/index.js
@@ -11,13 +11,21 @@ const app = express();
 const port = process.env.PORT | 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use(cors());
+app.options('*', (req, res) => {
+  res.status(200).json("OK");
+})
 
 app.use("/auth", userRouter);
 app.use("/api", apiRouter);
 
 app.get("/", (req, res) => {
-  res.json("Welcome to server!");
+  res.json("Welcome to server nottttttt!");
+});
+
+app.get("/test", (req, res) => {
+  res.json("You are in test");
 });
 
 mongoose
@@ -31,3 +39,5 @@ mongoose
   .catch((err) => {
     console.log({ err });
   });
+
+export default app;
